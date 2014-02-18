98dcdb46909bb459a79804b623d0eb96f2144501
Igor <igorbest@gmail.com>
2014-02-18 15:09:28 -0600

    init

--- app/views.py
+++ app/views.py
@@ -11,6 +11,7 @@ def index():
         #request conceptnet5
         term_field = form.terms.data
         terms = ','.join([x.strip() for x in term_field.split(',')]).replace(' ','_')
+        concept_req = ', '.join(terms.split(','))
         concept_url = 'http://conceptnet5.media.mit.edu/data/5.2/assoc/list/en/'+terms+'?limit=10&filter=/c/en'
         response = requests.get(concept_url)
         data = response.json()
@@ -19,26 +20,35 @@ def index():
         #print similar
         if len(similar) > 0:
             answers = [x[0].encode().split('/')[3] for x in similar]
-            keywords = [x for x in answers if x not in terms] 
+            keywords = [x for x in answers if x not in terms]
+            concept_ans = ', '.join(keywords)            
             google_q = keywords[0].replace('_','+')
             #request google images
             google_url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCqVvCxoKA6GKGcr_2kYqFg2O3E-kof2-c&cx=003867484209474582163:3ikgk4xehfo' +\
             '&searchType=image&fileType=jpg&imgSize=medium&alt=json&q='+google_q
             response = requests.get(google_url)
             data = response.json()
+            if u'error' in data:
+                return render_template('index.html',
+                    form = form,
+                    concept_req=concept_req,
+                    concept_ans = concept_ans,
+                    google_error = data[u'error'][u'errors'][0][u'message'],
+                    title = 'ConceptApp')               
             link = data[u'items'][0][u'link']
-            message = "ConceptNet5 association request: ("+ terms+"), results: "+', '.join(keywords)
-            return render_template('base.html',
+            return render_template('index.html',
                 form = form,
                 link = link,
                 image_title = keywords[0],
-                message = message,
+                concept_req= concept_req,
+                concept_ans = concept_ans,
                 title = 'ConceptApp')
         else:
-            return render_template('base.html',
+            return render_template('index.html',
                 form = form,
-                message = "No results from conceptnet5",
+                concept_req =concept_req,
+                concept_error = "No results from conceptnet5",
                 title = 'ConceptApp')
-    return render_template('base.html',
+    return render_template('index.html',
         form = form,
         title = 'ConceptApp')
